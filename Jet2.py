8df9ec80e07d778516e3e77c143798e80324ee8f
eason0215 <3379282637@qq.com>
2025-09-06 14:34:19 +0800

    Jet2 Holidays Meme
    Jet2 Holidays Meme

---
+++ Jet2.py
@@ -0,0 +1,12 @@
+from playsound import playsound
+
+lyrics = ("[Music]Darling hold my hand!\n","Nothing beats a Jet2 holiday\n","And right now you can save 50 Pounds per person\n","That's 200 Pounds off for a family of 4\n","We've got millions of free child place holidays available with 22kg of baggage included\n","Book now with jet2holidays\n","Package holidays you can trust\n")
+
+print(lyrics[0])
+print(lyrics[1])
+print(lyrics[2])
+print(lyrics[3])
+print(lyrics[4])
+print(lyrics[5])
+print(lyrics[6])
+playsound('Jet2WithAudio_out.mp3')
